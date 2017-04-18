89d040a56aba0bdf47d59bae531aa2a7751f8034
Eljam Russell <eljamrussell@gmail.com>
2017-04-18 11:20:14 -0400

    cp-2-is not complete

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+     function HomeCtrl() {
+         this.rooms = Room.all();
+         
+     }
+
+     angular
+         .module('blocChat')
+         .controller('HomeCtrl', ['Room', HomeCtrl]);
+ })();
