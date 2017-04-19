5dc9bf9c3949caa7e4aea3863b3fd1625fd081ee
Eljam Russell <eljamrussell@gmail.com>
2017-04-19 15:07:31 -0400

    cp-2-rooms fixed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,10 +1,12 @@
 (function() {
      function HomeCtrl() {
          this.rooms = Room.all();
-         
+
      }
 
      angular
          .module('blocChat')
          .controller('HomeCtrl', ['Room', HomeCtrl]);
  })();
+
+ 
