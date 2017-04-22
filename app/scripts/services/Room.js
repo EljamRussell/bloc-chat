b51bb10108fbe1c4a91c61f8ecc8541d1d447805
Eljam Russell <eljamrussell@gmail.com>
2017-04-22 13:12:02 -0400

    changes made

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,5 +1,4 @@
 (function() {
-<<<<<<< HEAD
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
@@ -11,23 +10,6 @@
     			}
     		};
     	}
-=======
-  function Room($scope, $firebaseArray) {
-    var ref = firebase.database().ref().child("rooms");
-    var rooms = $firebaseArray(ref);
-
-    var addRoom = function(room){
-       rooms.$add(room);
-     };
-
-    var room = {
-      all: rooms,
-      add: addRoom
-    }
-
-    return room;
-  }
->>>>>>> 6030984719fff07d3c21dc9d77559fbdc69eff79
 
   angular
     .module('blocChat')
