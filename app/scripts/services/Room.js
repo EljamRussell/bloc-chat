6030984719fff07d3c21dc9d77559fbdc69eff79
Eljam Sab Russell <eljamrussell@gmail.com>
2017-04-21 22:26:16 -0400

    Update Room.js

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -1,11 +1,8 @@
 (function() {
   function Room($scope, $firebaseArray) {
-      var Room = {};
-
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
 
-<<<<<<< HEAD
     var addRoom = function(room){
        rooms.$add(room);
      };
@@ -16,15 +13,6 @@
     }
 
     return room;
-=======
-    Room.all = rooms;
-
-    Room.add = function(room){
-       rooms.$add(room);
-};
-
-    return Room;
->>>>>>> ba6d2d12895b84368c3135951c80125b0c886d52
   }
 
   angular
